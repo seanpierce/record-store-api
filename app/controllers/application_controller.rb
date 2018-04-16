3ca9afd8e0c7617a5c57114841232916327fed5f
seanpierce <sumler.sean@gmail.com>
2018-04-16 06:31:04 -0700

    Add authentication handler to Action Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::API
-  after_action :set_version_header
-
-  protected
-    def set_version_header
-      response.set_header("Access-Control-Allow-Origin", "*")
-    end
+  acts_as_token_authentication_handler_for User, fallback: :none
+  # after_action :set_version_header
+  #
+  # protected
+  #   def set_version_header
+  #     response.set_header("Access-Control-Allow-Origin", "*")
+  #   end
 end
