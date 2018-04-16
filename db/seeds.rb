f73229c1d3969dbc95cbfdd64ea95d9811e68090
seanpierce <sumler.sean@gmail.com>
2018-04-15 18:01:05 -0700

    Add desctiption to Item seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@ class Seed
 
   def generate_items
     Item.destroy_all
+    puts "Destroyed all items..."
     20.times do |i|
       item = Item.create!(
         artist: Faker::Book.author,
         title: Faker::Book.title,
         cost: Random.rand(10...40),
-        image: "src/assets/items/#{Random.rand(1...8)}.jpg"
+        image: "src/assets/items/#{Random.rand(1...8)}.jpg",
+        description: Faker::Lorem.sentence(rand(20..50)).chomp('.')
       )
       puts "Item created!"
     end
