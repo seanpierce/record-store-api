a398d73099770a28abe899e6503a77ef03889e80
seanpierce <sumler.sean@gmail.com>
2018-04-15 18:05:37 -0700

    Add item details component

---
+++ record_store/src/ItemDetails.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'itemDetails',
+  data() {
+    return {
+      Item: {},
+      isLoading: true
+    }
+  },
+  methods: {
+  },
+  mounted: function() {
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    font-family: sans-serif;
+  }
+</style>
