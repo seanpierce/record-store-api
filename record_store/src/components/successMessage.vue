b745135232d2b3474a00c157d2dcf2fbaeb55630
seanpierce <sumler.sean@gmail.com>
2018-04-15 17:16:24 -0700

    Add successMessage component

---
+++ record_store/src/components/successMessage.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="success-message">
+  {{ message }}
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'successMessage',
+  data() {
+    return {
+      message: '',
+      show: false
+    }
+  },
+  methods: {
+    show(message) {
+      this.message = message
+      this.show = true
+      setTimeout(() => {
+        this.show = false
+      }, 3000)
+    }
+  },
+  mounted: function() {
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    font-family: sans-serif;
+  }
+  .success-message {
+    position: absolute;
+    top: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    background: green;
+    color: white;
+  }
+</style>
