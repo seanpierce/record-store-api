94179857c14df5dbcef761e0e16fdb69f7011f55
seanpierce <sumler.sean@gmail.com>
2018-04-18 06:54:39 -0700

    Only allow single resource routes for Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
-  resources :sessions, only: [:create, :destroy]
+  # note: singular resource for sessions
+  resource :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
