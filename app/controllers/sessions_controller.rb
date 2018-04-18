fdffbd6fea34da5de903f1d37eaf8934745f5c31
seanpierce <sumler.sean@gmail.com>
2018-04-18 06:54:12 -0700

    Maintain proper indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,21 @@
 class SessionsController < ApplicationController
 
-def create
-  user = User.where(email: params[:email]).first
+  def create
+    user = User.where(email: params[:email]).first
 
-  if user&.valid_password?(params[:password])
-    render json: user.as_json(only: [:email, :authentication_token]), status: :created
-  else
-    return head(:unauthorized)
+    if user&.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    else
+      head(:unauthorized)
+    end
   end
-end
 
-def destroy
-end
-
+  def destroy
+    current_user&.authentication_token = nil
+    if current_user.save
+      head(:ok)
+    else
+      head(:unauthorized)
+    end
+  end
 end
