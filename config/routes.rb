807f06111c55dadd2443b77e0da70ec188d19eb8
seanpierce <sumler.sean@gmail.com>
2018-04-18 20:51:24 -0700

    Add show route to Sessions for route authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   # note: singular resource for sessions
-  resource :sessions, only: [:create, :destroy]
+  resource :sessions, only: [:create, :destroy, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
